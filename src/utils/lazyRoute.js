8e6360c10c7ce45c93b9fcedb54567ea141e81df
wangjun <1099807917@qq.com>
2022-08-24 19:41:43 +0800

    优化login/404

---
+++ src/utils/lazyRoute.js
@@ -0,0 +1,18 @@
+import React, {lazy, Suspense} from 'react'
+import Auth from "./auth";
+
+const lazyRoute = (path, auth) => {
+    const Component = lazy(() => import(path))
+    return (
+        <Suspense fallback={<>loading...</>}>
+            {
+                auth ? <Auth>
+                    <Component/>
+                </Auth> : <Component/>
+            }
+
+        </Suspense>
+    )
+}
+
+export default lazyRoute
